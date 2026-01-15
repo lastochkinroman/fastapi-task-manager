40eb504a6b1b83f6166eafe36b634548db7abe07
Lastochkin <rdlastochkin@gmail.com>
2026-01-15 16:08:03 +0400

    feat: создание пользователя и первый эндпоинт регистрации

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
-from .database import engine, Base
-from . import models
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from .database import engine, Base, get_db
+from . import models, crud, schemas
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -9,3 +10,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return{"message": "Таблицы созданы, сервер работает!"}
+
+@app.post("/users/", response_model=schemas.UserOut)
+def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email = user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detai="Email уже зарегистрирован")
+    
+    return crud.create_user(db=db, user=user)
