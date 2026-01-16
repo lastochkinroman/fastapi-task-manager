c6a1cca01e73afec3b737d410f95c0287e03441c
Lastochkin <rdlastochkin@gmail.com>
2026-01-16 10:37:18 +0400

    refactor: замена passlib на bcrypt

--- app/crud.py
+++ app/crud.py
@@ -1,10 +1,13 @@
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models, schemas, utils
 
 def create_user(db: Session, user: schemas.UserCreate):
+
+    hashed_pass = utils.hash_password(user.password)
+
     db_user = models.User(
         email = user.email,
-        hashed_password = user.password
+        hashed_password = hashed_pass
     )
 
     db.add(db_user)
@@ -17,3 +20,10 @@ def create_user(db: Session, user: schemas.UserCreate):
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
--- app/main.py
+++ app/main.py
@@ -10,12 +10,24 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return{"message": "Таблицы созданы, сервер работает!"}
+    return{"message": "Таблицы созданы, сервер работает1!"}
 
 @app.post("/users/", response_model=schemas.UserOut)
 def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email = user.email)
     if db_user:
-        raise HTTPException(status_code=400, detai="Email уже зарегистрирован")
+        raise HTTPException(status_code=400, detail="Email уже зарегистрирован")
     
     return crud.create_user(db=db, user=user)
+
+@app.get("/users/", response_model=List[schemas.UserOut])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+@app.get("/users/{user_id}", response_model=schemas.UserOut)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+    return db_user
